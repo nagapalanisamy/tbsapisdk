8bccb24c0be386b608e7ffb53edf7d1d6e783170
stspeter <git+stspeter@spanenterprises.com>
2017-11-24 16:34:44 +0530

    New Project Added

---
+++ APIClientTool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(APIClientTool.Startup))]
+namespace APIClientTool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

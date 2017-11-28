75328ba9519a6d9518874bf6a861263b89b57cce
stspeter <git+stspeter@spanenterprises.com>
2017-11-28 12:16:23 +0530

    removed

--- APIClient.Core/Models/FormW2.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace APIClient.Core.Models
-{
-    public class FormW2
-    {
-        public BusinessDetails Business { get; set; }
-        public Employee Employee { get; set; }
-        public W2FormDetails FormDetails { get; set; }
-    }
-}

7f3b63871b0507cc9a981bb77d73696b7551f638
stskathir <git+kathir@spanenterprises.com>
2018-05-30 15:27:35 +0530

    Transmit comitted

--- sdk-dotnet/APIClientTool/Controllers/Form941SCHRController.cs
+++ sdk-dotnet/APIClientTool/Controllers/Form941SCHRController.cs
@@ -9,6 +9,7 @@ using Newtonsoft.Json;
 using APIClientTool.Utilities;
 using System.Net.Http;
 using System.Web.Script.Serialization;
+using APIClientTool.ViewModels;
 
 namespace APIClientTool.Controllers
 {
@@ -17,10 +18,126 @@ namespace APIClientTool.Controllers
         #region Form 941SCHR View Get Method
         // GET: Form941SCHR
         [Route("form941schr")]
-        public ActionResult Index()
+        public ActionResult Index(bool? id)
         {
             Form941SchRRecords form941SCHRRecords = new Form941SchRRecords();
+            bool _prePopulate = id ?? false;
+            if (_prePopulate)
+            {
+                PrePopulate(form941SCHRRecords);
+            }
             return View(form941SCHRRecords);
+        }
+        #endregion
+
+        #region Pre Populate
+        /// <summary>
+        /// Pre Populate date
+        /// </summary>
+        /// <param name="form941"></param>
+        private static void PrePopulate(Form941SchRRecords form941SchRRecords)
+        {
+            //Mapping Form 941 
+            form941SchRRecords.Sequence = "Record1";
+            form941SchRRecords.RecordId = null;
+            //Mapping Return Header
+            form941SchRRecords.ReturnHeader = new Form941SchRReturnHeader
+            {
+                ReturnType = null,
+                Qtr = "Q1",
+                TaxYr = "2018",
+                Business = new Business
+                {
+                    BusinessId = null,
+                    BusinessNm = "Test Business",
+                    EINorSSN = "003333330",
+                    IsEIN = true,
+                    BusinessType = "ESTE",
+                    ContactNm = "John Doe",
+                    Email = "employer@company.com",
+                    Fax = "1234567890",
+                    TradeNm = "Kodak",
+                    IsBusinessTerminated = false,
+                    Phone = "1234566890",
+                    PhoneExtn = "12345",
+                    IsForeign = false,
+                    USAddress = new USAddress
+                    {
+                        Address1 = "Address Line 1",
+                        City = "Rockhill",
+                        State = "SC",
+                        ZipCd = "29730"
+                    },
+                    SigningAuthority = new ViewModels.Form941CoreModel.SigningAuthority
+                    {
+                        BusinessMemberType = "ADMINISTRATOR",
+                        Phone = "1234564390",
+                        Name = "John"
+                    },
+                    KindOfEmployer = null,
+                    KindOfPayer = null,
+                    ForeignAddress = null
+                },
+                BusinessStatusDetails = new BusinessStatusDetails
+                {
+                    IsBusinessClosed = false,
+                    IsBusinessTransferred = false,
+                    IsSeasonalEmployer = false,
+                    BusinessClosedDetails = null,
+                    BusinessTransferredDetails = null
+                },
+                IsThirdPartyDesignee = true,
+                ThirdPartyDesignee = new ThirdPartyDesignee
+                {
+                    Name = "John Doe",
+                    Phone = "1234567890",
+                    PIN = "12345"
+                },
+                SignatureDetails = new SignatureDetails
+                {
+                    SignatureType = "ONLINE_SIGN_PIN",
+                    OnlineSignaturePIN = new OnlineSignaturePIN { PIN = "1234567890" }
+                }
+            };
+
+            //Mapping Return Data
+            form941SchRRecords.ReturnData = new Form941SchRReturnData
+            {
+                DepositScheduleType = new DepositScheduleType
+                {
+                    DepositorType = DepositorType.MONTHLY.ToString(),
+                    MonthlyDepositor = new MonthlyDepositor
+                    {
+                        TaxLiabilityMonth1 = 4,
+                        TaxLiabilityMonth2 = 345,
+                        TaxLiabilityMonth3 = 43
+                    }
+                },
+                AggregateForm941Data = new Form941Details
+                {
+                    EmployeeCnt = 3,
+                    WagesAmt = 5750000M,
+                    FedIncomeTaxWHAmt = 13499.76M,
+                    WagesNotSubjToSSMedcrTaxInd = null,
+                    Line5aInitialAmt = 57000M,
+                    Line5bInitialAmt = 0M,
+                    Line5cInitialAmt = 57500M,
+                    Line5dInitialAmt = 0M,
+                    Line5a = 7130M,
+                    Line5b = 0M,
+                    Line5c = 1667.50M,
+                    Line5d = 0M,
+                    Line5e = 8797.50M,
+                    TaxOnUnreportedTips3121qAmt = 0M,
+                    CurrentQtrFractionsCentsAmt = 0M,
+                    CurrentQuarterSickPaymentAmt = 0M,
+                    CurrQtrTipGrpTermLifeInsAdjAmt = 0M,
+                    Line12 = 22297.26M,
+                    Line11 = 0M,
+                    Line14 = 0M,
+                    Line15 = 0M
+                }
+            };
         }
         #endregion
 
@@ -106,5 +223,65 @@ namespace APIClientTool.Controllers
             return PartialView(form941SchRResponse);
         }
         #endregion
+
+        #region Transmit Return
+        /// <summary>
+        /// Function transmit the Form 941 Return to Efile
+        /// </summary>
+        /// <param name="submissionId">SubmissionId passed to transmit the 941 return</param>
+        /// <returns>TransmitFormW2Response</returns>
+        public ActionResult _TransmitReturn(Guid submissionId)
+        {
+            var transmitForm941 = new TransmitForm();
+            var transmitForm941SchRResponse = new TransmitForm941SchRResponse();
+            var transmitForm941SchRResponseJSON = string.Empty;
+            if (submissionId != null && submissionId != Guid.Empty)
+            {
+                // Getting the RecordIds for SubmissionId
+                transmitForm941 = APISession.GetRecordIdsBySubmissionId(submissionId);
+
+                // Generate JSON for TransmitForm 941
+                var requestJson = JsonConvert.SerializeObject(transmitForm941, Formatting.Indented);
+
+                if (transmitForm941 != null)
+                {
+                    using (var client = new PublicAPIClient())
+                    {
+                        //API URL to Transmit Form 941 Return
+                        string requestUri = "Form941SCHR/Transmit";
+
+                        //POST
+                        APIGenerateAuthHeader.GenerateAuthHeader(client, requestUri, "POST");
+
+                        //Get Response
+                        var _response = client.PostAsJsonAsync(requestUri, transmitForm941).Result;
+                        if (_response != null && _response.IsSuccessStatusCode)
+                        {
+                            //Read Response
+                            var createResponse = _response.Content.ReadAsAsync<TransmitForm941SchRResponse>().Result;
+                            if (createResponse != null)
+                            {
+                                transmitForm941SchRResponseJSON = JsonConvert.SerializeObject(createResponse, Formatting.Indented);
+                                transmitForm941SchRResponse = new JavaScriptSerializer().Deserialize<TransmitForm941SchRResponse>(transmitForm941SchRResponseJSON);
+                                if (transmitForm941SchRResponse.SubmissionId != null && transmitForm941SchRResponse.SubmissionId != Guid.Empty && transmitForm941SchRResponse.StatusCode == (int)StatusCode.Success)
+                                {
+                                    //Updating Filing Status (Transmitted) for a specific SubmissionId in Session 
+                                    APISession.UpdateFilingStatus(transmitForm941SchRResponse.SubmissionId);
+                                }
+                            }
+                        }
+                        else
+                        {
+                            var createResponse = _response.Content.ReadAsAsync<Object>().Result;
+                            transmitForm941SchRResponseJSON = JsonConvert.SerializeObject(createResponse, Formatting.Indented);
+                            transmitForm941SchRResponse = new JavaScriptSerializer().Deserialize<TransmitForm941SchRResponse>(transmitForm941SchRResponseJSON);
+                        }
+                    }
+                }
+            }
+            return PartialView(transmitForm941SchRResponseJSON);
+        }
+        #endregion
+
     }
 }

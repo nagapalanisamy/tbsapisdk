54a82e32ef7bcddb7544e3a8c82eb7b6d5a4fa09
sivakumark <sivakumar.kumarasamy@w3magix.com>
2018-05-30 17:43:10 +0530

    model changes

--- sdk-dotnet/APIClientTool/ViewModels/FormW2.cs
+++ sdk-dotnet/APIClientTool/ViewModels/FormW2.cs
@@ -5,7 +5,7 @@ namespace APIClientTool.ViewModels
     public class FormW2
     {
         public string Sequence { get; set; }
-        public BusinessDetails Business { get; set; }
+        public Business Business { get; set; }
         public Employee Employee { get; set; }
         public FormW2Details FormDetails { get; set; }
         public int TaxYear { get; set; }
